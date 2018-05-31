f99751106b34164520294a1e1a52ec4fb41bc8ea
Robert Whitney <xnite@xnite.me>
2018-05-31 00:50:09 -0500

    needed moar sysinfo

--- client/plugins/sysinfo.js
+++ client/plugins/sysinfo.js
@@ -4,6 +4,6 @@ kiwi.plugin('sysinfo', function(kiwi) {
 
     kiwi.state.$on("input.command.sysinfo", function(event,command,params){
         console.log(os);
-        kiwi.state.getActiveBuffer().say("\002Sysinfo:\002 [Platform: " + os.platform() + " " + os.release + " ] [Memory: " + ((os.totalmem - os.freemem)/1000000)/1024 + "GB free out of " + (os.totalmem/1000000)/1024 + "GB]");
+        kiwi.state.getActiveBuffer().say("\002Sysinfo:\002 [OS: " + os.platform() + " " +os.arch+ " " + os.release + " ] [CPU: "+os.cpus().length+"x - "+os.cpus()[0].model+"] [Memory: " + ((os.totalmem - os.freemem)/1000000)/1024 + "GB free out of " + (os.totalmem/1000000)/1024 + "GB]");
     });
 });

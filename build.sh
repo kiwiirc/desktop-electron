25d6ce1ce9bf4480e5328858a12ffce22114907e
Robert Whitney <xnite@xnite.me>
2018-05-31 17:30:12 -0500

    build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+git submodule init
+git submodule update
+cd ./kiwiirc/
+yarn install && yarn build
+mv ./dist ../client
+cd ..
+cp client-defaults/config.json client/static/config.json
+cp client-defaults/package.json client/package.json
+cp client-defaults/main.js client/main.js
+cp -r client-defaults/plugins client/plugins
+cd client/
+npm install

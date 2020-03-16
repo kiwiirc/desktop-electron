0cb4bc12158801f223f7c239090208164c3d6aa9
Darren <darren@darrenwhitlen.com>
2020-03-16 21:49:57 +0000

    Update irc-framework; Correctly use native TCP sockets

--- client-defaults/plugins/nativesocks.js
+++ client-defaults/plugins/nativesocks.js
@@ -1,10 +1,9 @@
 kiwi.plugin('nativesocks', function(kiwi) {
     
-    window.kiwi.state.$on("network.connecting", function(event)
-    {
+    kiwi.on("network.connecting", function(event){
         try
         {
-            event.network.ircClient.options.transport = require('irc-framework/src/transports/net');
+            event.transport = require('irc-framework/src/transports/net');
             console.log("Set DefaultTransport to native sockets.");
         } catch(e)
         {
